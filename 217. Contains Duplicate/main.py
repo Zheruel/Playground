a2935216079efecc4199cfbbfb3762fd16e3eed4
Zheruel <tinzeljar@gmail.com>
2022-01-24 20:13:29 +0100

    Contains duplicate with

---
+++ 217. Contains Duplicate/main.py
@@ -0,0 +1,19 @@
+from typing import List
+
+# This should be O(n) because hashmap
+
+
+def containsDuplicate(nums: List[int]) -> bool:
+    result_dict = {}
+
+    for num in nums:
+        if num in result_dict:
+            return True
+        else:
+            result_dict[num] = 1
+
+    return False
+
+
+nums = [1, 1, 2, 3, 4]
+print(containsDuplicate(nums))
