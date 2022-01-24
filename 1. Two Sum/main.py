f37e22c5101f697ec9d1694c7bd006065027d923
Zheruel <tinzeljar@gmail.com>
2022-01-24 04:26:03 +0100

    Add two sum

---
+++ 1. Two Sum/main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target):
+    hash_map = {nums[i]: i for i in range(len(nums))}
+    indices = []
+
+    for i, num in enumerate(nums):
+        search_target = target - num
+        hash_check = hash_map.get(search_target)
+
+        if hash_check != None and hash_check != i:
+            indices.append(i)
+            indices.append(hash_check)
+
+            return indices
+
+
+nums = [3, 3]
+target = 6
+
+print(twoSum(nums, target))
